1c1284577bb16403712ab196052071e1274c5a24
helloAbraham <abrahamemail@yahoo.com>
2023-07-02 15:57:27 -0400

    just added components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <InputFile v-model="name" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import InputFile from './components/InputFile';
 export default {
   name: 'App',
+  
   components: {
-    HelloWorld
-  }
+    InputFile
+  },
+  data(){
+   return {
+       name: '',
+    }
+  },
+  methods:{ }
 }
 </script>
 
---
+++ src/components/InputFile.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"/>
+</template>
+
+<script>
+    export default {
+        name: 'InputFile',
+        props: {
+            modelValue: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
